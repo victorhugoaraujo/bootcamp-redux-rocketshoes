e752bd47ba780990a9ce6cb19cc90e541efd7757
Victor Silva <victor.silva@endurance.com>
2019-08-05 20:08:34 -0300

    Styling Home and Cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { MdShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
 export default function Home() {
@@ -14,7 +14,91 @@ export default function Home() {
         <span>R$79,90</span>
 
         <button type="button">
-          <div />
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis nike</strong>
+        <span>R$79,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis nike</strong>
+        <span>R$79,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis nike</strong>
+        <span>R$79,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis nike</strong>
+        <span>R$79,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis nike</strong>
+        <span>R$79,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
         </button>
       </li>
     </ProductList>
